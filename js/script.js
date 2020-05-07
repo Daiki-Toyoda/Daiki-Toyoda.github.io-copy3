3e63ce174d6fc1e734b9ec2aa409893b36949606
Daiki-Toyoda <63102490+Daiki-Toyoda@users.noreply.github.com>
2020-05-07 23:03:53 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function() {
+
+$('.item-hover').hover(
+  function() {
+    $(this).find('.text-contents').addClass('text-active');
+
+},
+function() {
+    $(this).find('.text-contents').removeClass('text-active');
+
+}
+);
+
+
+
+});
